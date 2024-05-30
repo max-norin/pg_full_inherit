38636e96f3c7b227d09e0d710e28dd211315c7e0
max_norin <79068976350@yandex.ru>
2024-05-30 22:33:37 +0300

    add doc

--- event-triggers/drop_constraints_from_parent_tables.sql
+++ event-triggers/drop_constraints_from_parent_tables.sql
@@ -1,5 +1,4 @@
--- триггер события для добавление ограничений от родительских таблиц
--- в основом используется для создания REFERENCES
+-- триггер события для удаления ограничений от родительских таблиц
 CREATE FUNCTION public.event_trigger_drop_constraints_from_parent_tables ()
     RETURNS EVENT_TRIGGER
     AS $$
@@ -12,12 +11,11 @@ DECLARE
     "schema"               TEXT;
     "table"                TEXT;
 BEGIN
-    FOR object IN
+    FOR "object" IN
+    -- описание значений переменной object
+    -- https://www.postgresql.org/docs/current/functions-event-triggers.html#PG-EVENT-TRIGGER-SQL-DROP-FUNCTIONS
     SELECT * FROM pg_event_trigger_dropped_objects()
     LOOP
-        IF NOT "object"."original" THEN
-            CONTINUE;
-        END IF;
         -- удаление ограничений
         IF "object".object_type = 'table constraint' THEN
             "schema" = "object".address_names[1];
@@ -34,7 +32,7 @@ BEGIN
             -- https://www.postgresql.org/docs/current/catalog-pg-inherits.html
             SELECT inhrelid FROM pg_inherits WHERE inhparent = "parent"
             LOOP
-                -- имя ограничения в дочерней таблице
+                -- имя ограничения дочерней таблице
                 "name" = public.get_child_constraint_name("name", "parent"::REGCLASS::TEXT, "child"::REGCLASS::TEXT);
                 -- удаление ограничения name из дочерней таблицы
                 "query" = format('ALTER TABLE %1s DROP CONSTRAINT IF EXISTS %2s;', "child"::REGCLASS, "name");
--- event-triggers/drop_triggers_from_parent_tables.sql
+++ event-triggers/drop_triggers_from_parent_tables.sql
@@ -1,5 +1,4 @@
--- триггер события для добавление ограничений от родительских таблиц
--- в основом используется для создания REFERENCES
+-- триггер события для удаления триггеров от родительских таблиц
 CREATE FUNCTION public.event_trigger_drop_triggers_from_parent_tables ()
     RETURNS EVENT_TRIGGER
     AS $$
@@ -12,13 +11,12 @@ DECLARE
     "schema"               TEXT;
     "table"                TEXT;
 BEGIN
-    FOR object IN
+    FOR "object" IN
+    -- описание значений переменной object
+    -- https://www.postgresql.org/docs/current/functions-event-triggers.html#PG-EVENT-TRIGGER-SQL-DROP-FUNCTIONS
     SELECT * FROM pg_event_trigger_dropped_objects()
     LOOP
-        IF NOT "object"."original" THEN
-            CONTINUE;
-        END IF;
-        -- удаление ограничений
+        -- удаление триггера
         IF "object".object_type = 'trigger' THEN
             "schema" = "object".address_names[1];
             "table" = "object".address_names[2];
@@ -34,9 +32,9 @@ BEGIN
             -- https://www.postgresql.org/docs/current/catalog-pg-inherits.html
             SELECT inhrelid FROM pg_inherits WHERE inhparent = "parent"
             LOOP
-                -- имя триггера в дочерней таблице
+                -- имя триггера дочерней таблице
                 "name" = public.get_child_trigger_name("name", "parent"::REGCLASS::TEXT, "child"::REGCLASS::TEXT);
-                -- удаление ограничения name из дочерней таблицы
+                -- удаление триггер name из дочерней таблицы
                 "query" = format('DROP TRIGGER IF EXISTS %1s ON %2s;', "name", "child"::REGCLASS);
                 RAISE NOTICE USING MESSAGE = format('-- DROP TRIGGER %1s FROM %2s TABLE BASED ON DEPENDENCY ON %3s TABLE', "name", "child"::REGCLASS, "parent"::REGCLASS);
                 RAISE NOTICE USING MESSAGE = format("query");
