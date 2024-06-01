42938580895082cf92203f954c9504ed75b66cfa
max_norin <79068976350@yandex.ru>
2024-06-01 15:19:12 +0300

    fix test

--- test/drop.sql
+++ test/drop.sql
@@ -1,12 +1,12 @@
 -- удаление из таблицы пользователей UNIQUE (username)
 ALTER TABLE public.users
-    DROP CONSTRAINT users_username_ukey;
+    DROP CONSTRAINT "users--username: ukey";
 -- удаление из таблицы пользователей FOREIGN KEY (lang_id) REFERENCES langs (id)
 ALTER TABLE public.users
-    DROP CONSTRAINT users_lang_id_fkey;
+    DROP CONSTRAINT "users: lang_id_fkey";
 -- удаление из таблицы пользователей колонки с FOREIGN KEY (city_id) REFERENCES cities (id)
 ALTER TABLE public.users
     DROP COLUMN city_id;
 -- удаление из таблицы пользователей CONSTRAINT TRIGGER и TRIGGER
-DROP TRIGGER IF EXISTS "check_username" ON public.users;
-DROP TRIGGER IF EXISTS "auto_bio" ON public.users;
+DROP TRIGGER IF EXISTS "check username" ON public.users;
+DROP TRIGGER IF EXISTS "auto bio" ON public.users;
